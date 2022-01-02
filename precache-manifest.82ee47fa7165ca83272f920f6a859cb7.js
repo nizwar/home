3c5e36f9513be99773364d4d45d3371d3a8f105d
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2022-01-03 01:54:39 +0700

    Updates

---
+++ precache-manifest.82ee47fa7165ca83272f920f6a859cb7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89723f7b40e834001e3091957be6ab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "153fb588aff3c3ce05c6",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "7295c370075b06834268",
+    "url": "/static/js/2.c16f13e4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c16f13e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153fb588aff3c3ce05c6",
+    "url": "/static/js/main.0b3fd488.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "26b093a6fc099d3394e75da3fc6e3648",
+    "url": "/static/media/nettacode.26b093a6.png"
+  },
+  {
+    "revision": "9eeff3305d337e4676d134e5aaf5f1ef",
+    "url": "/static/media/nizwar.9eeff330.png"
+  },
+  {
+    "revision": "1a9efd922294d7df202049fe0f7cf8d8",
+    "url": "/static/media/oxxo.1a9efd92.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "3d23a83129991e41e0a7e6eed17990c8",
+    "url": "/static/media/wblue.3d23a831.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.5de77595a67c8c478f5910cf8006c619.js"
+  "/precache-manifest.82ee47fa7165ca83272f920f6a859cb7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
