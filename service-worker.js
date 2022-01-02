4a3167a94a6b5561967c7c6c78beae3fa8e92814
Mochamad Nizwar Syafuan <nizwar_id@icloud.com>
2022-01-03 02:17:22 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82ee47fa7165ca83272f920f6a859cb7.js"
+  "/precache-manifest.51589544385a23d03854bcb6241de08c.js"
 );
 
 self.addEventListener('message', (event) => {
